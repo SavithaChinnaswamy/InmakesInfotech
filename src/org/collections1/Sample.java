9961b30aa7b6428c4b3fa112fb13de1a416353d3
SavithaChinnaswamy <csavitha25@gmail.com>
2023-03-05 16:50:07 +0530

    collections

---
+++ src/org/collections1/Sample.java
@@ -0,0 +1,129 @@
+package org.collections1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Sample 
+{
+	public static void main(String[] args)
+	{
+	
+	Map<String, String> s1 = new HashMap<>();
+	
+	s1.put("studentid","001");
+	s1.put("name", "savitha");
+	s1.put("phone", "7619300600");
+	s1.put("address", "coimbatore");
+	s1.put("dob", "25-sep-1999");
+	s1.put("email", "csavitha25@gmail.com");
+	
+    Map<String, String> s2 = new HashMap<>();
+	
+	s2.put("studentid","002");
+	s2.put("name", "maya");
+	s2.put("phone", "7619300601");
+	s2.put("address", "madurai");
+	s2.put("dob", "16-jun-1999");
+	s2.put("email", "maya25@gmail.com");
+
+	
+	 Map<String, String> s3 = new HashMap<>();
+		
+		s3.put("studentid","003");
+		s3.put("name", "ramesh");
+		s3.put("phone", "7619300602");
+		s3.put("address", "trichy");
+		s3.put("dob", "16-aug-1999");
+		s3.put("email", "Ramesh25@gmail.com");
+		
+		
+		 Map<String, String> s4 = new HashMap<>();
+			
+			s4.put("studentid","004");
+			s4.put("name", "Tharani");
+			s4.put("phone", "7619300603");
+			s4.put("address", "shivagangai");
+			s4.put("dob", "17-mar-1999");
+			s4.put("email", "Thranai25@gmail.com");
+			
+			
+Map<String, String> s5 = new HashMap<>();
+			
+			s5.put("studentid","005");
+			s5.put("name", "Riyazz");
+			s5.put("phone", "7619300604");
+			s5.put("address", "shivagangai");
+			s5.put("dob", "19-mar-1999");
+			s5.put("email", "Riyazz25@gmail.com");
+			
+			
+			
+Map<String, String> s6 = new HashMap<>();
+			
+			s6.put("studentid","006");
+			s6.put("name", "Aashik");
+			s6.put("phone", "7619300605");
+			s6.put("address", "namakkal");
+			s6.put("dob", "22-dec-1999");
+			s6.put("email", "Aashik25@gmail.com");
+			
+		
+Map<String, String> s7 = new HashMap<>();
+			
+			s7.put("studentid","007");
+			s7.put("name", "shalini");
+			s7.put("phone", "7619300606");
+			s7.put("address", "shivagangai");
+			s7.put("dob", "11-dec-1999");
+			s7.put("email", "shalini25@gmail.com");
+		
+			
+			Map<String, String> s8 = new HashMap<>();
+						
+						s8.put("studentid","008");
+						s8.put("name", "vijay");
+						s8.put("phone", "7619300607");
+						s8.put("address", "madurai");
+						s8.put("dob", "22-nov-1999");
+						s8.put("email", "vijay25@gmail.com");
+					
+					
+	Map<String, String> s9 = new HashMap<>();
+						
+						s9.put("studentid","009");
+						s9.put("name", "milky");
+						s9.put("phone", "7619300608");
+						s9.put("address", "shivagangai");
+						s9.put("dob", "01-jan-1999");
+						s9.put("email", "milky@gmail.com");
+						
+						
+Map<String, String> s10 = new HashMap<>();
+						
+						s10.put("studentid","010");
+						s10.put("name", "milky");
+						s10.put("phone", "7619300608");
+						s10.put("address", "shivagangai");
+						s10.put("dob", "01-jan-1999");
+						s10.put("email", "milky@gmail.com");
+					
+			
+			
+		
+	
+	System.out.println("Details of student-1 are : "+s1);
+	System.out.println("Details of student-2 are : "+s2);
+	System.out.println("Details of student-3 are : "+s3);
+	System.out.println("Details of student-4 are : "+s4);
+	System.out.println("Details of student-5 are : "+s5);
+	System.out.println("Details of student-6 are : "+s6);
+	System.out.println("Details of student-7 are : "+s7);
+	System.out.println("Details of student-8 are : "+s8);
+	System.out.println("Details of student-9 are : "+s9);
+	System.out.println("Details of student-10 are : "+s10);
+
+	
+	}
+	
+}
+
